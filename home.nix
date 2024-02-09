f1227a74ffae7a1c45015e7142353135009f035d
Alexandra Deas <me@alexandradeas.co.uk>
2024-02-09 00:30:53 +0000

    add fzf & tree

--- home.nix
+++ home.nix
@@ -58,11 +58,16 @@ in {
     (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 		lua-language-server
 		jq
+		tree
+		fzf
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
     # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
