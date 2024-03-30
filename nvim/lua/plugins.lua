e460945885ba9a14903ad976b369523fa94739e4
Alexandra Deas <me@alexandradeas.co.uk>
2024-03-30 16:06:27 +0000

    feat: remove neotest
    Never bothered using it anyway

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -67,40 +67,6 @@ return require("lazy").setup({
 
 	{ "preservim/tagbar" },
 
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-neotest/neotest-go",
-		},
-		config = function()
-			-- get neotest namespace
-			local neotest_ns = vim.api.nvim_create_namespace("neotest")
-			vim.diagnostic.config({
-				virtual_text = {
-					format = function(diagnostic)
-						local message =
-								diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " ")
-								:gsub("^%s+", "")
-						return message
-					end,
-				},
-			}, neotest_ns)
-			require("neotest").setup({
-				adapters = {
-					require("neotest-go")({
-						experimental = {
-							test_table = true,
-						},
-						args = { "-count=1", "-timeout=60s" }
-					}),
-				},
-			})
-		end,
-	},
-
 	-- { 'codota/tabnine-nvim', build = "./dl_binaries.sh" },
 	{ 'zbirenbaum/copilot.lua' },
 	{
