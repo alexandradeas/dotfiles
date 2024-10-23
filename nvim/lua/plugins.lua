5cdd5d30411730b7b7b3c2efb740edd30905bed3
Alexandra Deas <me@alexandradeas.co.uk>
2024-10-23 13:48:52 +0100

    remove devdocs

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -101,50 +101,6 @@ return require("lazy").setup({
 		ft = { "json", "yaml" },
 	},
 	{
-		"luckasRanarison/nvim-devdocs",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		opts = {
-			dir_path = vim.fn.stdpath("data") .. "/devdocs", -- installation directory
-			telescope = {},                               -- passed to the telescope picker
-			filetypes = {
-				-- extends the filetype to docs mappings used by the `DevdocsOpenCurrent` command, the version doesn't have to be specified
-				-- scss = "sass",
-				-- javascript = { "node", "javascript" }
-			},
-			float_win = { -- passed to nvim_open_win(), see :h api-floatwin
-				relative = "editor",
-				height = 25,
-				width = 100,
-				border = "rounded",
-			},
-			wrap = false,   -- text wrap, only applies to floating window
-			previewer_cmd = "/usr/bin/glow",
-			cmd_args = {},  -- example using glow: { "-s", "dark", "-w", "80" }
-			cmd_ignore = {}, -- ignore cmd rendering for the listed docs
-			picker_cmd = true, -- use cmd previewer in picker preview
-			picker_cmd_args = { "-s", "dark", "-w", "50" },
-			mappings = {    -- keymaps for the doc buffer
-				open_in_browser = ""
-			},
-			ensure_installed = {
-				"dart-2",
-				"go",
-				"i3",
-				"node",
-				"jsdoc",
-				"ocaml",
-				"sqlite",
-				"fish-3.7",
-				"jekyll-4",
-			},
-			after_open = function(bufnr) end, -- callback that runs after the Devdocs window is opened. Devdocs buffer ID will be passed in
-		},
-	},
-	{
 		"nvim-treesitter/nvim-treesitter",
 		run = function()
 			require('tree-sitter.install').update({ with_sync = true })()
