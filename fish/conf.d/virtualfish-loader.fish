32e0ec4c7647cefc1f4ffb0197b40edcbcb8db55
Alexandra Deas <me@alexandradeas.co.uk>
2025-04-10 10:49:27 +0100

    fish: dependently load python venv based on hostname

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,13 @@
 set -g VIRTUALFISH_VERSION 2.5.9
-set -g VIRTUALFISH_PYTHON_EXEC /home/alexandra/.local/share/pipx/venvs/virtualfish/bin/python
-source /home/alexandra/.local/share/pipx/venvs/virtualfish/lib/python3.13/site-packages/virtualfish/virtual.fish
+
+switch (hostname)
+	case fedora
+		set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
+		source /home/alexandra/.local/lib/python3.13/site-packages/virtualfish/virtual.fish
+	case multivac
+		set -g VIRTUALFISH_PYTHON_EXEC /home/alexandra/.local/share/pipx/venvs/virtualfish/bin/python
+		source /home/alexandra/.local/share/pipx/venvs/virtualfish/lib/python3.13/site-packages/virtualfish/virtual.fish
+		emit virtualfish_did_setup_plugins
+end
+
 emit virtualfish_did_setup_plugins
